68ecf73e8022cc4a5aeaf4f0daf00b8c1baf8bff
Gowtham <56599573+gowthamgowthamp@users.noreply.github.com>
2022-04-01 23:16:23 +0530

    Add files via upload

---
+++ SUB16x8/genus.cmd
@@ -0,0 +1,15 @@
+# Cadence Genus(TM) Synthesis Solution, Version 17.21-s010_1, built Feb  7 2018
+
+# Date: Thu Aug 19 21:34:19 2021
+# Host: cad055 (x86_64 w/Linux 2.6.32-71.el6.x86_64) (2cores*4cpus*1physical cpu*Intel(R) Core(TM) i3-2120 CPU @ 3.30GHz 3072KB)
+# OS:   Red Hat Enterprise Linux Server release 6.0 (Santiago)
+
+set_db library /cad/FOUNDRY/digital/180nm/dig/lib/typical.lib
+read_hdl exact_sub.v
+elaborate
+synthesize -to_mapped -effort medium
+syn_opt
+report timing -unconstrained > timing_exact.txt
+report power > power_exact.txt
+report area > area_exact.txt
+exit
